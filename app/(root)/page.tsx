4c3e5eb5b1362ef0d2397fffec0909456e4ed2a5
Greekatos <dimmin99@gmail.com>
2026-07-17 19:37:07 +0300

    Youtube video at 1:34:50

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
-  const loggedIn = {firstName: 'Greekatos'};
+  const loggedIn = {firstName: 'Greekatos', lastName: 'Ellinas', email: 'wraioskai@gmail.com'};
 
 
   return (
@@ -23,7 +24,15 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance:500.50 }]}
+      />
     </section>
   )
 }
